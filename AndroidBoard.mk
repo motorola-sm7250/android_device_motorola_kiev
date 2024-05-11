ca9b14f366c2ed015e088398c8b1d62d9cb197c3
Andrew Hexen <SyberHexen@gmail.com>
2024-05-11 10:30:23 -0700

    Revert "kiev: Copy touch firmware to recovery"
    This reverts commit 04893fa622157ed44edb6bb9e6e5ad3866528e8b.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I6107735a6c346fcbf261b433b8ed118af808f1d7

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -32,16 +32,6 @@ $(INSTALLED_RECOVERY_KMOD_TARGETS): $(INSTALLED_KERNEL_TARGET)
 	@mkdir -p $(dir $@)
 	cp $(@F:%=$(TARGET_OUT_VENDOR)/lib/modules/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/
 
-RECOVERY_FIRMWARE_TARGETS := \
-    focaltech-csot-ft8756-05-0000-kiev.bin \
-    focaltech-tm-ft8756-07-0000-kiev.bin
-
-INSTALLED_RECOVERY_FIRMWARE_TARGETS := $(RECOVERY_FIRMWARE_TARGETS:%=$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/%)
-$(INSTALLED_RECOVERY_FIRMWARE_TARGETS): $(INSTALLED_KERNEL_TARGET)
-	echo -e ${CL_GRN}"Copying firmware to recovery"${CL_RST}
-	@mkdir -p $(dir $@)
-	cp $(@F:%=$(TARGET_OUT_VENDOR)/firmware/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/
-
 RECOVERY_SH_TARGETS := \
     load_touch.sh
 
@@ -51,4 +41,4 @@ $(INSTALLED_RECOVERY_SH_TARGETS): $(INSTALLED_KERNEL_TARGET)
 	@mkdir -p $(dir $@)
 	mv $(@F:%=$(TARGET_OUT_VENDOR)/bin/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/bin/
 
-$(recovery_uncompressed_ramdisk): $(INSTALLED_RECOVERY_KMOD_TARGETS) $(INSTALLED_RECOVERY_FIRMWARE_TARGETS) $(INSTALLED_RECOVERY_SH_TARGETS)
+$(recovery_uncompressed_ramdisk): $(INSTALLED_RECOVERY_KMOD_TARGETS) $(INSTALLED_RECOVERY_SH_TARGETS)
