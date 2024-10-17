4bd69371568ccb02e72a757f1f6783ad6887ff3f
tilt-rgb <lesma_chan@pm.me>
2024-10-17 02:09:14 +0000

    kiev: Introduce Dolby Atmos from xiaomi
    configure for laurel speaker: https://github.com/PowerX-NOT/hardware_dolby
    *Use QCOM implementation for audio effects
    *Add mediacodecs support (Useful for Apple Music)
    *Added shoebox effect
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -114,3 +114,5 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/kiev/kiev-vendor.mk)
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
